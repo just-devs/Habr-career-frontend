839f8f4a7fa3c6c381579f7aedd4d4abd57519d0
amangeldi0 <amanimanbekov12@gmail.com>
2023-01-21 21:26:41 +0600

    Menu icon

---
+++ assets/icon/Menu/Menu.tsx
@@ -0,0 +1,11 @@
+import styles from './Menu.module.scss';
+
+export const Menu = () => {
+  return (
+    <div className={styles.menu}>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  );
+};
